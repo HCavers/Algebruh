c28e6b85d97f3530af08724f6cec16dda259d53f
Hunter <huntercavers@xtra.co.nz>
2018-06-10 01:17:56 +1200

    Updated Tokenizer to check string for correct input (Note: Will not compile need to fix added function)

--- Tokenizer.java
+++ Tokenizer.java
@@ -19,4 +19,15 @@ class Tokenizer{
 		}
 		return tempString;
 	}
+	
+	private boolean checkCharSet(String input){
+		char[] charSet = {'a', 'b', 'c', '+', '-', '/', '*', '=', '(', ')'};
+		for(int i = 0; i < input.length(); i++){
+			if(!(charSet.contains(input.charAt(i)))){
+				System.out.println("Error: " + Character.toString(input.charAt(i)) + " is not a valid character.");
+				return false;
+			}
+		}
+		return true;
+	}
 }

9d74fd88ff206c8021230dcd559abb0a3b3dde4a
Hunter <huntercavers@xtra.co.nz>
2018-06-09 13:38:35 +1200

    Adding Tokenizer class with initial functionality of triming a string.

---
+++ Tokenizer.java
@@ -0,0 +1,22 @@
+class Tokenizer{
+	private static String _input;
+	private static char[] _output;
+	
+	public static void setInput(String input){
+		_input = input;
+	}
+
+	public static char[] getOutput(){
+		return _output;
+	}
+	
+	private String trimInput(){
+		String tempString = "";
+		for(int i = 0; i < _input.length(); i++){
+			if(_input.charAt(i) != ' '){
+				tempString += Character.toString(_input.charAt(i));
+			}
+		}
+		return tempString;
+	}
+}
